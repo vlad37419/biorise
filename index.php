aa02b0c3ad32579cdb4d958fc8469e3c85b9c238
vlad37419 <85108281+vlad37419@users.noreply.github.com>
2023-03-19 20:35:35 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/normalize.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>Biorise</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__container">
+            <a href="/" class="header__logo logo">
+                <img src="/assets/img/logo.svg" alt="Логотип клиники Biorise" class="logo__img">
+            </a>
+            <p class="header__text">
+                Клиника IV-терапии
+            </p>
+            <button class="header__btn btn">
+                Выбрать капельницу
+            </button>
+            <button class="header__modal-btn">
+                связаться с нами
+            </button>
+            <button class="header__menu-burger menu-burger">
+                <span class="menu-burger-line"></span>
+            </button>
+        </div>
+    </header>
+</body>
+
+</html>
